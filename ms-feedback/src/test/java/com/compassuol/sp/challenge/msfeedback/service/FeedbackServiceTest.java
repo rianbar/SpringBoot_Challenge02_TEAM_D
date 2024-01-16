d90b71122cf3fce74d5c8881281cae8cd228703c
rian <rianbarroso596@gmail.com>
2024-01-16 13:53:13 -0300

    ms-feedback tests bug fixed

--- ms-feedback/src/test/java/com/compassuol/sp/challenge/msfeedback/service/FeedbackServiceTest.java
+++ ms-feedback/src/test/java/com/compassuol/sp/challenge/msfeedback/service/FeedbackServiceTest.java
@@ -3,11 +3,13 @@ package com.compassuol.sp.challenge.msfeedback.service;
 import com.compassuol.sp.challenge.msfeedback.dto.FeedbackRequestDTO;
 import com.compassuol.sp.challenge.msfeedback.dto.FeedbackResponseDTO;
 import com.compassuol.sp.challenge.msfeedback.dto.OrderResponseDTO;
+import com.compassuol.sp.challenge.msfeedback.dto.ScaleEnum;
 import com.compassuol.sp.challenge.msfeedback.errors.BusinessErrorException;
 import com.compassuol.sp.challenge.msfeedback.errors.FeedbackNotFoundException;
 import com.compassuol.sp.challenge.msfeedback.model.FeedbackModel;
 import com.compassuol.sp.challenge.msfeedback.proxy.OrdersProxy;
 import com.compassuol.sp.challenge.msfeedback.repository.FeedbackRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -42,6 +44,29 @@ public class FeedbackServiceTest {
 
     @Mock
     TransferObjects transferObjects;
+
+    @BeforeEach
+    void setUp() {
+        feedbackRequestDTO = new FeedbackRequestDTO();
+        feedbackRequestDTO.setComment("test");
+        feedbackRequestDTO.setOrderId(1L);
+        feedbackRequestDTO.setScale(ScaleEnum.SATISFIED);
+
+        orderResponseDTO = new OrderResponseDTO();
+        orderResponseDTO.setStatus("CONFIRMED");
+
+        feedbackModel = new FeedbackModel();
+        feedbackModel.setScale(ScaleEnum.SATISFIED);
+        feedbackModel.setOrderId(1L);
+        feedbackModel.setComment("comment");
+        feedbackModel.setId(12L);
+
+        feedbackResponseDTO = new FeedbackResponseDTO();
+        feedbackResponseDTO.setId(1L);
+        feedbackResponseDTO.setOrderId(1L);
+        feedbackResponseDTO.setComment("very good my boy");
+        feedbackResponseDTO.setScale(ScaleEnum.SATISFIED);
+    }
 
     @Test
     void updateFeedbacks_withValidData_ReturnsFeedback() {
