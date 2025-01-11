579da17314c5c10b96884214eb64f3dd33682476
rianbar <rianbarroso596@gmail.com>
2025-01-11 19:44:47 -0300

    update spring version in microservices

--- ms-orders/src/main/java/com/compassuol/sp/challenge/msorders/model/OrderModel.java
+++ ms-orders/src/main/java/com/compassuol/sp/challenge/msorders/model/OrderModel.java
@@ -12,7 +12,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.text.ParseException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
