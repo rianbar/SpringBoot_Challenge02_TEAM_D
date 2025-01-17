f95e7bdfdc8eafdcdcd3a81661e505fef1eb65b4
rianbar <rianbarroso596@gmail.com>
2025-01-17 16:19:13 -0300

    fix: add path variable annotation

--- ms-orders/src/main/java/com/rian/ecommerce/challenge/msorders/proxy/AddressProviderClient.java
+++ ms-orders/src/main/java/com/rian/ecommerce/challenge/msorders/proxy/AddressProviderClient.java
@@ -2,11 +2,12 @@ package com.rian.ecommerce.challenge.msorders.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.rian.ecommerce.challenge.msorders.model.response.AddressResponse;
 
 @FeignClient(name = "ms-address-provider", url = "${provider.address.url}")
 public interface AddressProviderClient {
   @GetMapping("/{zipCode}/json/")
-  AddressResponse getAdderss(String zipCode);
+  AddressResponse getAdderss(@PathVariable String zipCode);
 }
